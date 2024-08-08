77c8af2a92fba0980cf6da3e0e985b72f138b1c0
shahbaz4783 <shahbaz4783@gmail.com>
2024-08-08 21:27:05 +0530

    bot message

--- api/bot.ts
+++ api/bot.ts
@@ -1,12 +1,27 @@
+import { Bot, InlineKeyboard, webhookCallback } from 'grammy';
 import 'dotenv/config';
-import { Bot, webhookCallback } from 'grammy';
 import express from 'express';
 
 const bot = new Bot(process.env.BOT_TOKEN!);
 
-bot.command('start', (ctx) => {
-	console.log('Received /start command');
-	ctx.reply(`Welcome ${ctx.from?.first_name}`);
+await bot.api.setMyCommands([
+	{ command: 'start', description: 'Start the bot' },
+	{ command: 'help', description: 'Show help text' },
+	{ command: 'report', description: 'Report' },
+]);
+
+bot.command('start', async (ctx) => {
+	const keyboard = new InlineKeyboard()
+		.url('Open Mini App', 'https://t.me/bot_nextjs_bot/start')
+		.row()
+		.text('Explore Guide', 'explore_guide')
+		.row()
+		.text('Tap to Earn', 'tap_to_earn');
+
+	await ctx.replyWithPhoto('https://grammy.dev/images/grammY.png', {
+		caption: 'Welcome to TapSwap! Tap on the buttons below to get started.',
+		reply_markup: keyboard,
+	});
 });
 
 bot.command('help', (ctx) => {
@@ -17,12 +32,10 @@ bot.command('help', (ctx) => {
 const app = express();
 app.use(express.json());
 
-// Set up the webhook
 app.use(webhookCallback(bot, 'express'));
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, async () => {
+app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}`);
 });
-
--- build/bot.js
+++ build/bot.js
@@ -1,10 +1,23 @@
+import { Bot, InlineKeyboard, webhookCallback } from 'grammy';
 import 'dotenv/config';
-import { Bot, webhookCallback } from 'grammy';
 import express from 'express';
 const bot = new Bot(process.env.BOT_TOKEN);
-bot.command('start', (ctx) => {
-    console.log('Received /start command');
-    ctx.reply(`Welcome ${ctx.from?.first_name}`);
+await bot.api.setMyCommands([
+    { command: 'start', description: 'Start the bot' },
+    { command: 'help', description: 'Show help text' },
+    { command: 'report', description: 'Report' },
+]);
+bot.command('start', async (ctx) => {
+    const keyboard = new InlineKeyboard()
+        .url('Open Mini App', 'https://t.me/bot_nextjs_bot/start')
+        .row()
+        .text('Explore Guide', 'explore_guide')
+        .row()
+        .text('Tap to Earn', 'tap_to_earn');
+    await ctx.replyWithPhoto('https://grammy.dev/images/grammY.png', {
+        caption: 'Welcome to TapSwap! Tap on the buttons below to get started.',
+        reply_markup: keyboard,
+    });
 });
 bot.command('help', (ctx) => {
     console.log('Received /help command');
@@ -12,17 +25,8 @@ bot.command('help', (ctx) => {
 });
 const app = express();
 app.use(express.json());
-// Set up the webhook
-const secretPath = `/webhook/${bot.token}`;
-app.use(secretPath, webhookCallback(bot, 'express'));
+app.use(webhookCallback(bot, 'express'));
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, async () => {
+app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-    try {
-        await bot.api.setWebhook(`${process.env.URL}${secretPath}`);
-        console.log('Webhook set successfully');
-    }
-    catch (error) {
-        console.error('Error setting webhook:', error);
-    }
 });
